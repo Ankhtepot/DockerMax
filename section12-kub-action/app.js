fe2cc8c0613b5881bcb7cfd2ec00754b7bf65c61
PetrZavodny <petr.zavodny@chyronhego.com>
2021-07-22 07:52:20 +0200

    Up to lesson 211.

--- section12-kub-action/app.js
+++ section12-kub-action/app.js
@@ -4,8 +4,7 @@ const app = express();
 
 app.get('/', (req, res) => {
   res.send(`
-    <h1>Hello from this NodeJS app!!!!</h1>
-    <p>This is new.</p>
+    <h1>!Hello from this NodeJS app!</h1>
     <p>Try sending a request to /error and see what happens</p>
   `);
 });
